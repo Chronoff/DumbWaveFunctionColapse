1085afeb56de833b2898ad5f1207c188c6ae73ce
Chronoff <adam_jarousek@Brejchostroj.>
2025-02-03 09:53:48 +0100

    i added pepa commment

--- dumWaveFuncCol.cpp
+++ dumWaveFuncCol.cpp
@@ -206,7 +206,7 @@ int main()
 	
 	field1.printField();	
         field1.clear();
-	std::cout << "%%%%%%%%%%%%wow i changed this" << std::endl;
+	std::cout << "%%%%%%%%%%%%pepa" << std::endl;
 	for (int i = 0; i < 1000; i++)
 	{
 		field1.calcAllCells();
